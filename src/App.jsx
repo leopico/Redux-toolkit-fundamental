a2cebefbc168bc66d79eaa2c47e719b4d0b722a9
leopico <linkoko.peceng@gmail.com>
2023-08-06 11:06:52 +0630

    simple with counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
+import "./slices/counterSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { increase, decrease, reset } from "./slices/counterSlice";
+
 function App() {
+  const { count } = useSelector((store) => store.counterous);
+  const dispatch = useDispatch();
   return (
-    <>
-      <h1 className="text-2xl font-light text-green-400">Hello world!</h1>
-    </>
+    <div>
+      <h1 className="text-2xl font-bold text-center">Count: {count}</h1>
+      <div className="flex justify-center space-x-3 pt-3">
+        <button
+          className="bg-gray-200 rounded px-2 py-1"
+          onClick={() => dispatch(increase("this is increase"))}
+        >
+          Increase +
+        </button>
+        <button
+          className="bg-gray-200 rounded px-2 py-1"
+          onClick={() => dispatch(decrease([1, 2, 3]))}
+        >
+          decrease -
+        </button>
+        <button
+          className="bg-gray-200 rounded px-2 py-1"
+          onClick={() =>
+            dispatch(
+              reset({
+                one: "this is one",
+                two: "this is two",
+                three: "this is three",
+              })
+            )
+          }
+        >
+          Reset{" "}
+        </button>
+      </div>
+    </div>
   );
 }
 
